7059a3e53e23767aacf8d7715a16ed5ea8a5c196
Lee Ui Hwan <luh5063014@naver.com>
2025-09-10 17:08:22 +0900

    Feat: 플레이어&상점 오브젝트 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 플레이어 컨트롤러
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private Vector2 movementInput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        // Normalize to prevent faster diagonal movement
+        movementInput.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 newPosition = rb.position + movementInput * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
+    }
+}
