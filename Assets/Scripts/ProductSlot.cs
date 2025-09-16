608fa08134511916400dc7d114ac0dc716b0a42e
Lee Ui Hwan <luh5063014@naver.com>
2025-09-16 15:36:53 +0900

    Feat: 상점 시스템 구현

--- Assets/Scripts/ProductSlot.cs
+++ Assets/Scripts/ProductSlot.cs
@@ -4,15 +4,20 @@ using UnityEngine.UI;
 public class ProductSlot : MonoBehaviour
 {
     [Header("Component")]
-    [SerializeField] private Image productSprite;
-    [SerializeField] private Text productName;
-    [SerializeField] private Text productPrice;
-    [SerializeField] private Text productCount;
-    [SerializeField] private Text totalPrice;
+    [SerializeField] private Image productSprite;       // 상품 이미지
+    [SerializeField] private Text productNameTxt;       // 상품 이름
+    [SerializeField] private Text productPriceTxt;      // 상품 가격
+    [SerializeField] private Text productCountTxt;      // 상품 수량
+    [SerializeField] private Text totalPriceTxt;        // 구매 금액
+    [SerializeField] private Text purchaseCountTxt;     // 구매 수량
 
     [Header("Item")]
     public Item item;
-    
+
+    [Header("Info")]
+    private int totalPrice;          // 현재 구매하려는 총 금액
+    private int purchaseCount;       // 현재 구매하려는 수량
+
     // 슬롯 갱신
     public void SlotUpdate(Item _item)
     {
@@ -23,9 +28,51 @@ public class ProductSlot : MonoBehaviour
 
         item = _item;
 
-        productName.text = item.name;
-        productCount.text = "x" + item.count.ToString();
-        productPrice.text = item.price.ToString();
+        productNameTxt.text = item.name;
+        productCountTxt.text = "x" + item.count.ToString();
+        productPriceTxt.text = item.price.ToString();
         if(item.sprite != null) productSprite.sprite = (Sprite)item.sprite;
+
+        purchaseCount = (item.count > 0) ? 1 : 0;
+        totalPrice = item.price * purchaseCount;
+
+        totalPriceTxt.text = totalPrice.ToString();
+        purchaseCountTxt.text = "x" + purchaseCount.ToString();
+    }
+
+    // 상품 추가
+    public void Increse()
+    {
+        if (purchaseCount >= item.count) return;
+
+        purchaseCount++;
+        totalPrice = item.price * purchaseCount;
+
+        totalPriceTxt.text = totalPrice.ToString();
+        purchaseCountTxt.text = "x" + purchaseCount.ToString();
+    }
+
+    // 상품 제거
+    public void Decrese()
+    {
+        if (purchaseCount < 0) return;
+
+        purchaseCount--;
+        totalPrice = item.price * purchaseCount;
+
+        totalPriceTxt.text = totalPrice.ToString();
+        purchaseCountTxt.text = "x" + purchaseCount.ToString();
+    }
+
+    // 상품 구매
+    public void Purchase()
+    {
+        // 사용자가 구매할 수 있는지 체크
+        if (ShopManager.Insatnce.TryPurchase(totalPrice) == false) return;
+
+        // 수량 체크
+        if (purchaseCount <= 0) return;
+
+        ShopManager.Insatnce.Purchase(item, purchaseCount);
     }
 }
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,18 +1,30 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
     public static ShopManager Insatnce;
 
-    private int MAX_PRODUCTSLOT = 4;    // 상품 리스트 최대 개수
+    [Header("ProductArea")]
+    private int MAX_PRODUCTSLOT = 4;                                                                // 상품 리스트 최대 개수
 
-    [SerializeField] private int category;  // 상점 카테고리
-    [SerializeField] private Transform listParent;  // 상품 리스트 위치
-    [SerializeField] private GameObject productSlotPrefab; // 상품 슬롯 프리팹
-    [SerializeField] private List<GameObject> productSlotList = new List<GameObject>(); // 상품 리스트
-    [SerializeField] private List<Item> productList = new List<Item>(); // 상품 리스트
+    [SerializeField] private int category;                                                          // 상점 카테고리
+    [SerializeField] private Transform listParent;                                                  // 상품 리스트 위치
+    [SerializeField] private GameObject productSlotPrefab;                                          // 상품 슬롯 프리팹
+    [SerializeField] private List<GameObject> productSlotList = new List<GameObject>();             // 상품 슬롯 리스트
+    [SerializeField] private List<Item> productList = new List<Item>();                             // 상품 리스트
+
+    [SerializeField] private Text pageTxt;                                                          // 상품 페이지
+
+    [Header("PlayerArea")]
+    [SerializeField] private Text userMoneyTxt;                                                     // 플레이어 보유 금액
+
+    [Header("Value")]
+    private int userMoney = 10000;
+    private int maxPage;
+    private int currentPage;
 
     private void Awake()
     {
@@ -30,6 +42,8 @@ public class ShopManager : MonoBehaviour
     void Start()
     {
         InitProductList();
+
+        userMoneyTxt.text = userMoney.ToString();
     }
 
     // 상품 리스트 초기화
@@ -39,26 +53,80 @@ public class ShopManager : MonoBehaviour
         Item[] loadItems = Resources.LoadAll<Item>("Items");
         productList = new List<Item>(loadItems);
 
-        // 4가지 아이템 슬롯을 생성 및 리스트 저장
-        for (int i=0; i< productList.Count; i++)
-        {
-            if (i == MAX_PRODUCTSLOT) break;
+        // 페이지 초기화
+        currentPage = 1;
+        maxPage = productList.Count / 4 + 1;
 
+        // 상품 슬롯 4개 생성
+        for(int i=0; i<MAX_PRODUCTSLOT; i++)
+        {
             GameObject newProductSlot = Instantiate(productSlotPrefab, listParent);
-            newProductSlot.GetComponent<ProductSlot>().SlotUpdate(productList[i]);
             productSlotList.Add(newProductSlot);
         }
+
+        UpdateProductList();
     }
 
     // 상품 리스트 갱신
     public void UpdateProductList()
     {
-        
+        // 범위 설정
+        int startIndex = (currentPage - 1) * 4;
+        int endIndex = startIndex + MAX_PRODUCTSLOT;
+        int slotIndex = 0;
+
+        // 4가지 아이템 슬롯을 생성 및 리스트 저장
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            if (i >= productList.Count || productList[i] == null) break;
+
+            productSlotList[slotIndex].GetComponent<ProductSlot>().SlotUpdate(productList[i]);
+            productSlotList[slotIndex].SetActive(true);
+            slotIndex++;
+        }
+
+        // 빈 상품 슬롯 비활성화
+        for(int i=slotIndex; i < MAX_PRODUCTSLOT; i++)
+        {
+            productSlotList[i].SetActive(false);
+        }
+
+        // 상품 페이지 갱신
+        pageTxt.text = currentPage.ToString() + "/" + maxPage.ToString();
+    }
+
+    // 구매할 수 있는지 체크
+    public bool TryPurchase(int totalPrice)
+    {
+        return (totalPrice <= userMoney);
     }
 
     // 구매하기
-    public void Purchase(Item item)
+    public void Purchase(Item item, int purchaseCount)
     {
         // ItemSlot에서 가져온 리스트 구매
+        item.count -= purchaseCount;
+        userMoney -= item.price * purchaseCount;
+        userMoneyTxt.text = userMoney.ToString();
+
+        // 유저의 인벤토리에 itme 추가
+
+        UpdateProductList();
+    }
+
+    public void Prev()
+    {
+        if (currentPage <= 1) return;
+
+        currentPage--;
+        UpdateProductList();
+    }
+
+    public void Next()
+    {
+        if (currentPage >= maxPage) return;
+
+        currentPage++;
+        UpdateProductList();
     }
 }
